51686308aa9e121317f757afaf4bb70766659c98
Paul Erpenstein <paul.erpenstein@gmail.com>
2025-04-15 21:13:23 +0200

    Add simple tests for transform

--- MonoGameExample.Tests/Scene/TransformTests.cs
+++ MonoGameExample.Tests/Scene/TransformTests.cs
@@ -1,5 +1,4 @@
 // implement tests for Transform class
-using System;
 using Microsoft.Xna.Framework;
 using MonoGameExample.Scene;
 using Xunit;
@@ -19,6 +18,72 @@ namespace MonoGameExample.Tests.Scene
 
             // Assert
             Assert.Equal(Matrix.Identity, worldMatrix);
+        }
+
+        [Fact]
+        public void TestTransformTranslation()
+        {
+            // Arrange
+            var transform = new Transform(null);
+            var translation = new Vector3(10, 20, 30);
+
+            // Act
+            transform.Position = translation;
+            transform.UpdateWorldMatrix(null);
+            var worldMatrix = transform.WorldMatrix;
+
+            // Assert
+            Assert.True(Matrix.CreateTranslation(translation).Equals(worldMatrix));
+        }
+
+        [Fact]
+        public void TestTransformRotation()
+        {
+            // Arrange
+            var transform = new Transform(null);
+            var rotation = Quaternion.CreateFromAxisAngle(Vector3.Up, MathHelper.ToRadians(90));
+
+            // Act
+            transform.Rotation = rotation;
+            transform.UpdateWorldMatrix(null);
+            var worldMatrix = transform.WorldMatrix;
+
+            // Assert
+            Assert.True(Matrix.CreateFromQuaternion(rotation).Equals(worldMatrix));
+        }
+
+        [Fact]
+        public void TestTransformScaling()
+        {
+            // Arrange
+            var transform = new Transform(null);
+            var scale = new Vector3(2, 2, 2);
+
+            // Act
+            transform.Scale = scale;
+            transform.UpdateWorldMatrix(null);
+            var worldMatrix = transform.WorldMatrix;
+
+            // Assert
+            Assert.True(Matrix.CreateScale(scale).Equals(worldMatrix));
+        }
+
+        [Fact]
+        public void TestTransformParentChild()
+        {
+            // Arrange
+            var parentTransform = new Transform(null);
+            var childTransform = new Transform(null);
+            var translation = new Vector3(10, 20, 30);
+
+            // Act
+            parentTransform.Position = translation;
+            parentTransform.UpdateWorldMatrix(null);
+            childTransform.UpdateWorldMatrix(parentTransform);
+            var worldMatrix = childTransform.WorldMatrix;
+
+            // Assert
+            Assert.True(Matrix.CreateTranslation(translation).Equals(worldMatrix));
         }
     }
 }
